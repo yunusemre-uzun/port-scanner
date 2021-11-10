70aa26bc63d11a6038ba1fe93c64c74e38629679
Yunus Emre Uzun <yunusemre.uzun@kobil.com>
2021-11-10 17:45:36 +0300

    bug fix

--- Elasticsearch/Elk/elk_controller.py
+++ Elasticsearch/Elk/elk_controller.py
@@ -6,36 +6,34 @@ import json
 class ElkController:
 
     _settings_json = {
-            "settings": {
-                "number_of_shards": 1,
-                "number_of_replicas": 0
-            },
-            "mappings": {
-                "members": {
-                    "dynamic": "strict",
-                    "properties": {
-                        "name": {
-                            "type": "text"
-                        },
-                        "result": {
-                            "type": "nested"
-                        },
-                        "error": {
-                            "type": "text"
-                        }
-                    }
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+        },
+        "mappings": {
+            "properties": {
+                "name": {
+                    "type": "text"
+                },
+                "result": {
+                    "type": "nested"
+                },
+                "error": {
+                    "type": "text"
                 }
             }
         }
-    _index_name = "scan_results"
+    }
+    _index_name = "scan_result"
     def __init__(self) -> None:
         self.__connect_elasticsearch()
         if self._connected:
-            self.__create_index()
+            is_created = self.create_index()
+            logging.info("creating index: {}".format(is_created))
     
     def __connect_elasticsearch(self):
         logging.info("trying to connect elasticseach")
-        self._elk = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
+        self._elk = Elasticsearch([{"host": "elasticsearch", "port": 9200}])
         self._connected = self.__connectionCheckElasticsearch()
     
     def __connectionCheckElasticsearch(self) -> bool:
@@ -46,15 +44,18 @@ class ElkController:
             logging.info("could not connect to elasticseach")
             return False
     
-    def __create_index(self) -> bool:
+    def create_index(self) -> bool:
         created = False
         try:
+            logging.info("creating index. current indices: {}".format(self._elk.indices.exists(self._index_name)))
             if not self._elk.indices.exists(self._index_name):
                 # Ignore 400 means to ignore "Index Already Exist" error.
-                self._elk.indices.create(index=self._index_name, ignore=400, body=self.settings)
-                logging.info('new index created with name {}'.format(self._index_name))
+                logging.info("Settings {}".format(self._settings_json))
+                response = self._elk.indices.create(index=self._index_name, ignore=400, body=self._settings_json)
+                logging.info("new index created with response {}".format(response))
             created = True
         except Exception as ex:
+            logging.error(ex)
             raise ex
         finally:
             return created
@@ -64,7 +65,7 @@ class ElkController:
             self.__connect_elasticsearch()
         try:
             logging.info("adding message with value: {}".format(message.value))
-            response = self._elk.index(index='scan_result', id=uuid4(), body=json.dumps(message.value)) # Index new data
+            response = self._elk.index(index="scan_result", id=uuid4(), body=json.dumps(message.value)) # Index new data
             logging.info(response)
         except Exception as ex:
             logging.error(ex)
